6f000e216a720676a6541f5277901048ea58099c
Fabricio <fabricioliquesley15@gmail.com>
2024-09-11 21:51:17 -0300

    feat: create a function to access the get pending goals route

---
+++ rocketseat-nlw/NLW-Pocket/web/src/http/get-pending-goals.ts
@@ -0,0 +1,14 @@
+type getPendingGoalsResponse = {
+  id: string;
+  title: string;
+  desiredWeeklyFrequency: number;
+  completionCount: number;
+}[];
+
+export async function getPendingGoals(): Promise<getPendingGoalsResponse> {
+  const response = await fetch('http://localhost:3333/pending-goals');
+
+  const data = await response.json();
+
+  return data.pendingGols;
+}
