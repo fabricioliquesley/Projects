446213997f8a24a8c8fded9d535a48d644170647
Fabricio <fabricioliquesley15@gmail.com>
2024-10-08 20:00:53 -0300

    feat: create screen layout

---
+++ link-manager/src/app/_layout.tsx
@@ -0,0 +1,9 @@
+import { colors } from "@/styles/colors";
+import { Stack } from "expo-router";
+
+export default function Layout() {
+  return <Stack screenOptions={{
+    headerShown: false,
+    contentStyle: { backgroundColor: colors.gray[950] }
+  }}/>
+}
