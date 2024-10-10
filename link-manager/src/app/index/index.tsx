dba18e295567b9701e3e88626c174a9821548cfe
Fabricio <fabricioliquesley15@gmail.com>
2024-10-10 15:55:30 -0300

    feat: create a modal to show link details

--- link-manager/src/app/index/index.tsx
+++ link-manager/src/app/index/index.tsx
@@ -1,10 +1,11 @@
-import { Image, TouchableOpacity, View, FlatList } from "react-native";
+import { Image, TouchableOpacity, View, FlatList, Modal, Text } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
 import { styles } from "./styles";
 import { colors } from "@/styles/colors";
 import { Categories } from "@/components/categories";
 import { Link } from "@/components/link";
+import { Option } from "@/components/option";
 
 export default function Index() {
   return (
@@ -34,6 +35,31 @@ export default function Index() {
         contentContainerStyle={styles.linksContent}
         showsVerticalScrollIndicator={false}
       />
+
+      <Modal transparent visible={true}>
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalCategory}>Curso</Text>
+              <TouchableOpacity>
+                <MaterialIcons name="close" size={20} color={colors.gray[400]}/>
+              </TouchableOpacity>
+            </View>
+
+            <Text style={styles.modaLinkName}>
+              Rocketseat
+            </Text>
+            <Text style={styles.modaLinkUrl}>
+              https://www.rocketseat.com.br
+            </Text>
+
+            <View style={styles.modalFooter}>
+              <Option title="Excluir" icon="delete" variant="secondary"/>
+              <Option title="Abrir" icon="link" />
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   )
 }
--- link-manager/src/app/index/styles.ts
+++ link-manager/src/app/index/styles.ts
@@ -34,5 +34,53 @@ export const styles = StyleSheet.create({
     gap: 20,
     padding: 24,
     paddingBottom: 100
+  },
+
+  modal: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+
+  modalContent: {
+    backgroundColor: colors.gray[900],
+    borderTopWidth: 1,
+    borderTopColor: colors.gray[800],
+    padding: 24,
+    paddingBottom: 32
+  },
+
+  modalHeader: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    marginBottom: 42
+  },
+
+  modalCategory: {
+    flex: 1,
+    color: colors.gray[400],
+    fontSize: 16,
+    fontWeight: "500"
+  },
+  
+  modaLinkName: {
+    color: colors.gray[200],
+    fontSize: 18,
+    fontWeight: "600"
+  },
+
+  modaLinkUrl: {
+    color: colors.gray[400],
+    fontSize: 14
+  },
+
+  modalFooter: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+    marginTop: 32,
+    borderTopWidth: 1,
+    borderTopColor: colors.gray[600],
+    paddingVertical: 14
   }
 })
