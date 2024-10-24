5b9098ad9e4e3988e32587bd5f95d4a8505b8dac
Fabricio <fabricioliquesley15@gmail.com>
2024-10-24 17:22:28 -0300

    feat: create user controller

--- simple-to-do-msaas/src/server.ts
+++ simple-to-do-msaas/src/server.ts
@@ -1,12 +1,19 @@
-import express from "express";
+import express from 'express';
+import {
+	createUserController,
+	findOneUserController,
+	listUserController,
+} from './controllers/user.controller';
 
 const app = express();
 const port = 3000;
 
-app.get("/", (_, res) => {
-  res.send("Hello world!");
-})
+app.use(express.json());
+
+app.get('/users', listUserController);
+app.get('/users/:userId', findOneUserController);
+app.post('/users', createUserController);
 
 app.listen(port, () => {
-  console.log(`server is running on http://localhost:${port}`);
-})
+	console.log(`server is running on http://localhost:${port}`);
+});
