2cf97088bc6f4d22de229a2b1ea61f41c405ad58
Fabricio <fabricioliquesley15@gmail.com>
2024-10-24 19:29:32 -0300

    feat: create task controller

--- simple-to-do-msaas/src/server.ts
+++ simple-to-do-msaas/src/server.ts
@@ -4,6 +4,7 @@ import {
 	findOneUserController,
 	listUserController,
 } from './controllers/user.controller';
+import { createTaskController } from './controllers/task.controller';
 
 const app = express();
 const port = 3000;
@@ -13,6 +14,7 @@ app.use(express.json());
 app.get('/users', listUserController);
 app.get('/users/:userId', findOneUserController);
 app.post('/users', createUserController);
+app.post('/tasks', createTaskController);
 
 app.listen(port, () => {
 	console.log(`server is running on http://localhost:${port}`);
