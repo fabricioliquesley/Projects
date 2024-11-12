2b107aae427855a387eb043fba40cc58be8224f2
Fabricio <fabricioliquesley15@gmail.com>
2024-11-12 19:39:49 -0300

    add prettier formater to the project

--- memora/app/page.tsx
+++ memora/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-full">
-      <h1>Hello World, this is Memora</h1> 
+    <div className="flex h-full items-center justify-center">
+      <h1>Hello World, this is Memora</h1>
     </div>
   );
 }
