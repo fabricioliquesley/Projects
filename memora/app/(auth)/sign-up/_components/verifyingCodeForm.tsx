9fc37a3c5d7daea116d3158910636fa12d0ffc9a
Fabricio <fabricioliquesley15@gmail.com>
2024-11-20 09:53:02 -0300

    feat: create the option to resend the code on the code verification page

--- memora/app/(auth)/sign-up/_components/verifyingCodeForm.tsx
+++ memora/app/(auth)/sign-up/_components/verifyingCodeForm.tsx
@@ -18,10 +18,12 @@ import { useSignUp } from "@clerk/nextjs";
 import { isClerkAPIResponseError } from "@clerk/nextjs/errors";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
+
+const ONE_SECOND = 1000;
 
 const verifyingCodeFormSchema = z.object({
   code: z.string().min(6, {
@@ -42,6 +44,8 @@ export function VerifyingCodeForm() {
   const router = useRouter();
 
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isReadyToResend, setIsReadyToResend] = useState(false);
+  const [totalTime, setTotalTime] = useState(60);
 
   async function handleVerifyingCode(data: VerifyingCodeFormSchema) {
     if (!isLoaded) return;
@@ -74,6 +78,42 @@ export function VerifyingCodeForm() {
     }
   }
 
+  async function resendCode() {
+    try {
+      await signUp?.prepareEmailAddressVerification({
+        strategy: "email_code",
+      });
+
+      toast.message("The new code has been sent to your email address");
+
+      setTotalTime(60);
+      setIsReadyToResend(false);
+    } catch (error) {
+      if (isClerkAPIResponseError(error)) {
+        for (const err of error.errors) {
+          toast.error(err.message);
+        }
+      } else {
+        toast.error("something went wrong, try again!");
+        console.error(JSON.stringify(error, null, 2));
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (totalTime == 0) {
+      setIsReadyToResend(true);
+      return;
+    }
+
+    const decrementTimer = setTimeout(() => {
+      console.log("decrement");
+      setTotalTime((prev) => prev - 1);
+    }, ONE_SECOND);
+
+    return () => clearTimeout(decrementTimer);
+  }, [totalTime]);
+
   return (
     <Form {...verifyingCodeForm}>
       <form onSubmit={verifyingCodeForm.handleSubmit(handleVerifyingCode)}>
@@ -99,6 +139,16 @@ export function VerifyingCodeForm() {
           />
         </div>
         <CardFooter className="flex justify-end pt-4">
+          <Button
+            variant={"ghost"}
+            type="button"
+            disabled={!isReadyToResend}
+            onClick={resendCode}
+          >
+            {isReadyToResend
+              ? "Resend code"
+              : `Resend code in ${totalTime.toString().padStart(2, "0")}`}
+          </Button>
           <Button type="submit" disabled={isSubmitting}>
             {isSubmitting ? <Loading /> : "Check code"}
           </Button>
