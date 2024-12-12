efa83c1d6b22b1ff29203b535f716fc6679a0915
Fabricio <fabricioliquesley15@gmail.com>
2024-12-12 10:27:32 -0300

    feat: linking the welcome screen to Home screen

--- rocketseat-nlw/nlw-pocket-mobile/mobile/src/app/index.tsx
+++ rocketseat-nlw/nlw-pocket-mobile/mobile/src/app/index.tsx
@@ -1,13 +1,24 @@
 import { View } from "react-native";
+import { router } from "expo-router";
 
 import { Welcome } from "@/components/Welcome";
 import { Steps } from "@/components/steps";
+import { Button, } from "@/components/Button";
 
 export default function Home() {
   return (
     <View style={{flex: 1, padding: 40, gap: 40}}>
       <Welcome />
       <Steps />
+      <Button onPress={navigateToHome}>
+        <Button.Title>
+          Começar
+        </Button.Title>
+      </Button>
     </View>
   )
+
+  function navigateToHome() {
+    router.navigate("/home");
+  }
 }
