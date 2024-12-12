2365d96b888f1f9bec498f3ec03da6f8b34defbb
Fabricio <fabricioliquesley15@gmail.com>
2024-12-12 12:06:44 -0300

    feat: create a type to Place

--- rocketseat-nlw/nlw-pocket-mobile/mobile/src/utils/types.ts
+++ rocketseat-nlw/nlw-pocket-mobile/mobile/src/utils/types.ts
@@ -2,3 +2,12 @@ export type Category = {
   id: string, 
   name: string
 };
+
+export type Place = {
+  id: string
+  name: string
+  description: string
+  coupons: number
+  cover: string
+  address: string
+}
