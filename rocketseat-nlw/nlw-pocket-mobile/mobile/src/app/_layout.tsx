88d9469ca9303a5754e75a98b001a6bcddeba653
Fabricio <fabricioliquesley15@gmail.com>
2024-12-12 12:10:04 -0300

    feat: add GestureHandlerRootView component in layout

--- rocketseat-nlw/nlw-pocket-mobile/mobile/src/app/_layout.tsx
+++ rocketseat-nlw/nlw-pocket-mobile/mobile/src/app/_layout.tsx
@@ -6,6 +6,7 @@ import {
   Rubik_500Medium,
   Rubik_700Bold
 } from "@expo-google-fonts/rubik";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import { COLORS } from "@/styles/theme";
 import { Loading } from "@/components/Loading";
@@ -23,11 +24,13 @@ export default function Layout() {
   }
 
   return (
-    <Stack 
-      screenOptions={{
-        headerShown: false,
-        contentStyle: { backgroundColor: COLORS.gray[100] }
-      }}
-    />
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack 
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: COLORS.gray[100] }
+        }}
+      />
+    </GestureHandlerRootView>
   )
 }
